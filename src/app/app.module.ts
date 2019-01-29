54d77e6cf3cb91a8f4a5ba666d38b4df5cdf231e
Bernat77 <bernatsalleraslopez@hotmail.com>
2019-01-29 17:43:54 +0100

    14 exercise

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,12 +16,14 @@ import { MatListModule } from '@angular/material/list';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
